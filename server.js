72647d5c9e0b49950d0444164a2214178b4b28c1
nodev918 <devnode77@gmail.com>
2021-08-21 00:53:38 +0800

    11

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 5555;
+// const PORT = process.env.PORT || 5000;
+
+// app.get("/", (req, res) => {
+//     res.send("Hello from server")
+// });
+
+if(process.env.NODE_ENV == "production"){
+  app.use(express.static('client/build'))
+  const path = require('path')
+  app.get("*",(req,res) => {
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
+
+app.listen(PORT, () => {
+    console.log("Server is listening on ", PORT)
+});
